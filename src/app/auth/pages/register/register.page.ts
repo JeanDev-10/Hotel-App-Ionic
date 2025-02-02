57fa910303d95f2ba5b90bff836cbe08e4889dc8
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-02 11:16:46 -0500

    style: :lipstick: register page ready

--- src/app/auth/pages/register/register.page.ts
+++ src/app/auth/pages/register/register.page.ts
@@ -1,20 +1,40 @@
+import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { eyeOffOutline, eyeOutline, lockClosed, arrowForwardCircle, eye, add, personAddOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    NavbarComponent,
+  ],
 })
 export default class RegisterPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.registerIcons();
   }
 
+  ngOnInit() {}
+  private registerIcons() {
+    addIcons({
+      eyeOffOutline,
+      eyeOutline,
+      lockClosed,
+      arrowForwardCircle,
+      eye,
+      add,
+      personAddOutline,
+    });
+  }
 }
