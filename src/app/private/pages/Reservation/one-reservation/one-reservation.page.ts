e5f499936d71d9ed05298cd444fb5b8ffdb67923
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-02 14:50:57 -0500

    style: :lipstick: one reservation ready

--- src/app/private/pages/Reservation/one-reservation/one-reservation.page.ts
+++ src/app/private/pages/Reservation/one-reservation/one-reservation.page.ts
@@ -1,20 +1,84 @@
+import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-one-reservation',
   templateUrl: './one-reservation.page.html',
   styleUrls: ['./one-reservation.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, NavbarComponent,RouterModule],
 })
 export default class OneReservationPage implements OnInit {
+  isAdmin:boolean=true;
+  reservation = {
+    id: 2,
+    date_start: '2025-01-30T22:16:45.000Z',
+    date_end: '2025-01-30T22:16:45.000Z',
+    price_total: 20,
+    user_id: 2,
+    room_id: 3,
+    status_id: 1,
+    user: {
+      id: 2,
+      name: 'jean',
+      lastname: 'rodriguez',
+      email: 'jean@hotmail.com',
+      password: '$2b$10$SqtYX3MYB8/5Unu6fefjB.aRoIRpNuF0s5klv22kz0GiTRVspNI4C',
+      role_id: 2,
+    },
+    status_reservation: {
+      id: 1,
+      name: 'Pendiente',
+    },
+    room: {
+      id: 3,
+      name: 'asdasd',
+      description: 'asdasdas',
+      price: 22,
+      type_id: 1,
+      types_room: {
+        id: 1,
+        name: 'Estándar',
+      },
+      images: [
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+      ],
+    },
+  };
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+
+  // Cancelar reservación
+  async cancelReservation(reservationId: number) {
+
   }
-
 }
