40e70cbb4348373cb86309ab5d668003395c925b
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-02 10:58:22 -0500

    style: :lipstick: login pages ready

--- src/app/auth/pages/login/login.page.ts
+++ src/app/auth/pages/login/login.page.ts
@@ -1,20 +1,32 @@
+import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavbarComponent } from '../../../shared/components/navbar/navbar.component';
+import { arrowForwardCircle, eyeOffOutline, eyeOutline, personAddOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ IonicModule,CommonModule, FormsModule, NavbarComponent,RouterModule],
 })
 export default class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.registerIcons();
   }
 
+  ngOnInit() {}
+
+  private registerIcons() {
+    addIcons({
+      eyeOffOutline,
+      eyeOutline,
+      personAddOutline,
+      arrowForwardCircle,
+    });
+  }
 }
