86bab14cfd30ec8c0dcc54f534b5d75ed7e99d00
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-03 16:53:42 -0500

    feat: :safety_vest: form of register page ready

--- src/app/auth/pages/register/register.page.ts
+++ src/app/auth/pages/register/register.page.ts
@@ -1,11 +1,13 @@
 import { IonicModule } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { eyeOffOutline, eyeOutline, lockClosed, arrowForwardCircle, eye, add, personAddOutline } from 'ionicons/icons';
+import { ToastService } from 'src/app/core/services/toast.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -18,14 +20,73 @@ import { eyeOffOutline, eyeOutline, lockClosed, arrowForwardCircle, eye, add, pe
     CommonModule,
     FormsModule,
     NavbarComponent,
+    ReactiveFormsModule
   ],
 })
 export default class RegisterPage implements OnInit {
+  private toastService = inject(ToastService);
+  private fb = inject(FormBuilder);
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
+  registerForm!: FormGroup;
+  IsPassword: boolean = false;
+  IsConfirmationPassword: boolean = false;
   constructor() {
     this.registerIcons();
+    this.registerRegisterForm();
   }
 
+
+  SeePassword(text: string) {
+    if (text == 'password') {
+      this.IsPassword = !this.IsPassword;
+    } else {
+      this.IsConfirmationPassword = !this.IsConfirmationPassword;
+    }
+  }
   ngOnInit() {}
+  getformHasError(field: string, rule: string): boolean | undefined {
+    return this.registerForm.get(field)?.hasError(rule);
+  }
+  getFormHasTouch(field: string): boolean | undefined {
+    return this.registerForm.get(field)?.touched;
+  }
+  private registerRegisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        name: ['', [Validators.required, Validators.minLength(5)]],
+        lastname: ['', [Validators.required, Validators.minLength(5)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+      },
+    );
+  }
+  onSubmit() {
+    if (this.registerForm.valid) {
+      /**
+       * ! ENVIAR PETICIÓN HTTP
+       *  */
+      console.log(this.registerForm.value);
+      this._authService.register(this.registerForm.value).subscribe({
+        next: (data) => {
+          console.log(data);
+          this.toastService.presentToastSucess(
+            '¡Credenciales creadas exitosamente!'
+          );
+          this._router.navigateByUrl('/auth/login');
+        },
+      });
+    } else {
+      this.toastService.presentToastError('¡Formulario invalido!');
+    }
+  }
   private registerIcons() {
     addIcons({
       eyeOffOutline,
