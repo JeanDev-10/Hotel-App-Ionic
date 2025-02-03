0c791480a584024e565a4ca074bca5563dcabc20
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-02 22:33:54 -0500

    style: :lipstick: get rooms page ready

---
+++ src/app/private/pages/Room/components/room-filter/room-filter.component.ts
@@ -0,0 +1,23 @@
+import { IonicModule } from '@ionic/angular';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-room-filter',
+  standalone:true,
+  imports: [IonicModule,CommonModule,FormsModule],
+  templateUrl: './room-filter.component.html',
+  styleUrls: ['./room-filter.component.scss'],
+})
+export class RoomFilterComponent  {
+  selectedType: string = ''; // Tipo de habitación seleccionado
+
+  @Output() filterChange = new EventEmitter<string>(); // Emite el tipo seleccionado
+
+  // Método que se ejecuta al cambiar el filtro
+  onFilterChange() {
+    this.filterChange.emit(this.selectedType);
+  }
+
+}
--- src/app/private/pages/Room/rooms/rooms.page.ts
+++ src/app/private/pages/Room/rooms/rooms.page.ts
@@ -1,20 +1,147 @@
+import { IonicModule } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
+import { RoomFilterComponent } from '../components/room-filter/room-filter.component';
+import { RoomCardComponent } from '../components/room-card/room-card.component';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.page.html',
   styleUrls: ['./rooms.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonicModule,
+    NavbarComponent,
+    RoomFilterComponent,
+    RoomCardComponent,
+    CommonModule,RouterModule
+  ],
 })
 export default class RoomsPage implements OnInit {
+  rooms: any = [
+    {
+      id: 1,
+      name: 'Room 101',
+      description: 'Habitación de lujo',
+      price: 200,
+      types_room: {
+        id: 1,
+        name: 'Suite',
+      },
+      images: [
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Room 102',
+      description: 'Habitación estándar',
+      price: 100,
+      types_room: {
+        id: 2,
+        name: 'Estándar',
+      },
+      images: [
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+      ],
+    },
+  ]; // Todas las habitaciones
+  filteredRooms: any[] = [
+    {
+      id: 1,
+      name: 'Room 101',
+      description: 'Habitación de lujo',
+      price: 200,
+      types_room: {
+        id: 1,
+        name: 'Suite',
+      },
+      images: [
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Room 102',
+      description: 'Habitación estándar',
+      price: 100,
+      types_room: {
+        id: 2,
+        name: 'Estándar',
+      },
+      images: [
+        {
+          id: 11,
+          url: 'https://hotelvictoriachone.com/wp-content/uploads/2022/12/HABITACION-MATRIMONIAL-2-HOTEL-VICTORIA-CHONE-1750x1000.jpg',
+          public_id: 'asdsada',
+          room_id: 3,
+        },
+      ],
+    },
+  ]; // Habitaciones filtradas
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onFilterChange(type: string) {
+    if (type) {
+      this.filteredRooms = this.rooms.filter(
+        (room: any) => room.types_room.name === type
+      );
+    } else {
+      this.filteredRooms = this.rooms; // Mostrar todas las habitaciones si no hay filtro
+    }
   }
-
 }
