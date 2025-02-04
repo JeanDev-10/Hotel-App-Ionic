f8776cb781d75ad501c1d06ead422d8fe7eeb554
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-04 12:31:00 -0500

    fix: :lipstick: buttons of actions in one rooms page

--- src/app/private/pages/Room/components/room-actions/room-actions.component.ts
+++ src/app/private/pages/Room/components/room-actions/room-actions.component.ts
@@ -2,20 +2,25 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HasRoleDirective } from 'src/app/core/directives/hasRole.directive';
+import { addIcons } from 'ionicons';
+import { checkboxOutline, closeOutline, pencilOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-room-actions',
-  standalone:true,
-  imports:[CommonModule,IonicModule,HasRoleDirective],
+  standalone: true,
+  imports: [CommonModule, IonicModule, HasRoleDirective],
   templateUrl: './room-actions.component.html',
   styleUrls: ['./room-actions.component.scss'],
 })
-export class RoomActionsComponent  {
+export class RoomActionsComponent {
   @Output() edit = new EventEmitter<void>(); // Evento para editar
   @Output() delete = new EventEmitter<void>(); // Evento para eliminar
   @Output() reserve = new EventEmitter<void>(); // Evento para reservar
 
   // Métodos para emitir eventos
+  constructor(){
+    this.registerIcons()
+  }
   onEdit() {
     this.edit.emit();
   }
@@ -27,5 +32,11 @@ export class RoomActionsComponent  {
   onReserve() {
     this.reserve.emit();
   }
-
+  private registerIcons() {
+    addIcons({
+      checkboxOutline,
+      closeOutline,
+      pencilOutline
+    });
+  }
 }
