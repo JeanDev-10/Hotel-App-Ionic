fb0679c832be9001b321c67a7f20303b24f121e5
JeanDev-10 <jeanpierrerodriguezzambrano@gmail.com>
2025-02-04 12:11:14 -0500

    feat: :sparkles: get user data from api or localstorage ready

--- src/app/private/pages/User/my-profile/my-profile.page.ts
+++ src/app/private/pages/User/my-profile/my-profile.page.ts
@@ -1,11 +1,12 @@
 import { IonicModule } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from '../../../../shared/components/navbar/navbar.component';
 import { ProfileComponent } from "../components/profile/profile.component";
 import { ChangePasswordComponent } from "../components/change-password/change-password.component";
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-my-profile',
@@ -15,19 +16,34 @@ import { ChangePasswordComponent } from "../components/change-password/change-pa
   imports: [IonicModule, CommonModule, FormsModule, NavbarComponent, ProfileComponent, ChangePasswordComponent],
 })
 export default class MyProfilePage implements OnInit {
+  private readonly _authService=inject(AuthService);
   segment: string = 'profile';
-  user = {
-    name: 'Juan',
-    lastName: 'Pérez',
-    email: 'juan.perez@example.com',
-    role: {
-      name:"cliente"
-    },
-  };
+  user!:any;
+
   segmentChanged(event: any) {
     this.segment = event.detail.value;
   }
-  constructor() {}
+  constructor() {
+
+  }
+  /*
+  * usar metodo para cargar los datos al entrar a la vista en todos las conexiones a la api
+  */
+  ionViewWillEnter(){
+    this.getUserData()
+  }
+
+  private getUserData(){
+    this._authService.getMe().subscribe({
+      next:(data)=>{
+        console.log(data)
+        this.user=data;
+      },
+      error:(error)=>{
+        console.log(error)
+      }
+    })
+  }
 
   ngOnInit() {}
 }
